ed43e3ab36db58017ff230f48683f11d7ea05b0f
Houssem eddine ben arbia <hbenarbia@tn.bulldozerinc.com>
2020-08-26 13:49:49 +0100

    Update node version

--- server/test/auth.spec.js
+++ server/test/auth.spec.js
@@ -1,64 +1,80 @@
-const HttpStatus =  require('http-status-codes');
-const {validUser, userWithInvalidPassword, userWithEmptyFisrtName,
-     invalidCredentials, validCredentials} = require('./mocks/user')
+const HttpStatus = require("http-status-codes");
+const {
+  validUser,
+  userWithInvalidPassword,
+  userWithEmptyFisrtName,
+  invalidCredentials,
+  validCredentials,
+} = require("./mocks/user");
 
-const { AssertGetRequest, AssertPostRequest, AssertSecuredGetRequest} = require ('./utils/setup');
+const {
+  AssertGetRequest,
+  AssertPostRequest,
+  AssertSecuredGetRequest,
+} = require("./utils/setup");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 describe("Authentication controller", () => {
-    describe("#SignUp", () => {
-        before((done) => {
-            User.deleteMany({}, (err) => { 
-               if(err) throw err;   
-                done();
-            });        
-        });
-        it('Should return 400 when passing a not valid password', (done) => {
-            AssertPostRequest('/auth/register', userWithInvalidPassword, HttpStatus.BAD_REQUEST, done)
-        })
-        
-        it('Should return 400 when passing an empty value for firstName', (done) => {
-            AssertPostRequest('/auth/register', userWithEmptyFisrtName, HttpStatus.BAD_REQUEST, done)
-        })
+  describe("#SignUp", () => {
+    before(async () => {
+      await User.deleteMany({});
+    });
+    it("Should return 400 when passing a not valid password", (done) => {
+      AssertPostRequest(
+        "/auth/register",
+        userWithInvalidPassword,
+        HttpStatus.BAD_REQUEST,
+        done
+      );
+    });
 
-        it('Should return 201 after creating a new user', (done) => {
-            AssertPostRequest('/auth/register',validUser, HttpStatus.CREATED, done)
-        })
+    it("Should return 400 when passing an empty value for firstName", (done) => {
+      AssertPostRequest(
+        "/auth/register",
+        userWithEmptyFisrtName,
+        HttpStatus.BAD_REQUEST,
+        done
+      );
+    });
 
-        it('Should return 409 after passing an exist user', (done) => {
-            AssertPostRequest('/auth/register',validUser, HttpStatus.CONFLICT, done)
-        })
-      
-    })
+    it("Should return 201 after creating a new user", (done) => {
+      AssertPostRequest("/auth/register", validUser, HttpStatus.CREATED, done);
+    });
 
-    describe('#Login', () => {
-        before((done) => {
-            User.deleteMany({}, (err) => { 
-                if(err) throw err;   
-             });        
-             User.create(validUser).then(() => done());
-        })
-        it('Should return 401 after log in with  invalid credentials', (done) => {
-            AssertPostRequest('/auth/login', invalidCredentials, HttpStatus.UNAUTHORIZED, done)
-        })
-        it('Should return 200 after log in a valid user', (done) => {
-            AssertPostRequest('/auth/login', validCredentials, HttpStatus.OK, done)
-        })
-    })
+    it("Should return 409 after passing an exist user", (done) => {
+      AssertPostRequest("/auth/register", validUser, HttpStatus.CONFLICT, done);
+    });
+  });
 
-    describe('#Me Get current connected user', () => {
-        before((done) => {
-            User.deleteMany({}, (err) => { 
-                if(err) throw err;   
-             });        
-             User.create(validUser).then(() => done());
-        })
-        it('Should return 403 when trying to get data with not connected user', (done) => {
-            AssertGetRequest('/auth/me', HttpStatus.FORBIDDEN, done)
-        })
-        it('Should return 200 http status code', (done) => {
-            AssertSecuredGetRequest('/auth/me', HttpStatus.OK, done);
-        })
-    })
+  describe("#Login", () => {
+    before(async () => {
+      await User.deleteMany({});
+      await User.create(validUser);
+    });
+    it("Should return 401 after log in with  invalid credentials", (done) => {
+      AssertPostRequest(
+        "/auth/login",
+        invalidCredentials,
+        HttpStatus.UNAUTHORIZED,
+        done
+      );
+    });
+    it("Should return 200 after log in a valid user", (done) => {
+      AssertPostRequest("/auth/login", validCredentials, HttpStatus.OK, done);
+    });
+  });
+
+  describe("#Me Get current connected user", () => {
+    before(async () => {
+      await User.deleteMany({});
+      await User.create(validUser);
+    });
+    it("Should return 403 when trying to get data with not connected user", (done) => {
+      AssertGetRequest("/auth/me", HttpStatus.FORBIDDEN, done);
+    });
+    it("Should return 200 http status code", (done) => {
+      AssertSecuredGetRequest("/auth/me", HttpStatus.OK, done);
+    });
+  });
 });
