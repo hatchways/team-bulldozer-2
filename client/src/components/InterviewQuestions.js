09a0beaa5001e94b24dcfba277d5a1156062fe64
Mehdi karray <mkarray@bulldozerinc.com>
2020-09-11 12:52:57 +0100

    ADD: add question content and title

--- client/src/components/InterviewQuestions.js
+++ client/src/components/InterviewQuestions.js
@@ -1,13 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        padding: "5px 15px",
+    },
+    title: {
+        color: "#516BF6",
+        fontWeight: 600,
+        marginBottom: 10,
+        marginTop: 20,
     },
     content: {
-        padding: "5px 15px",
         '@global': {
             p: {
                 fontWeight: 500,
@@ -28,19 +34,26 @@ const InterviewQuestions = (props) => {
     const { showContent, content } = props;
     return (
         <div className={classes.root}>
-            {showContent && (
-                <div className={classes.content} dangerouslySetInnerHTML={{
-                    __html: content
-                }}>
 
+            {showContent && (
+                <div>
+                    <Typography component="h5" variant="h5" className={classes.title}>
+                        {content.title}
+                    </Typography>
+                    <div className={classes.content} dangerouslySetInnerHTML={{
+                        __html: content.body
+                    }}>
+
+                    </div>
                 </div>
+
             )}
         </div>
     );
 };
 
 InterviewQuestions.propTypes = {
-    content: PropTypes.string,
+    content: PropTypes.object,
     showContent: PropTypes.bool,
 };
 
--- client/src/pages/Interview.js
+++ client/src/pages/Interview.js
@@ -145,8 +145,8 @@ const Interview = () => {
   const [codeValue, setCodeValue] = useState();
   const [stream, setStream] = useState();
   const [interview, setInterview] = useState();
-  const [yourQuestionContent, setYourQuestionContent] = useState("<p>Given an array of integers <code>nums</code>&nbsp;and an integer <code>target</code>, return <em>indices of the two numbers such that they add up to <code>target</code></em>.</p><p>You may assume that each input would have <strong><em>exactly</em> one solution</strong>, and you may not use the <em>same</em> element twice.</p><p>You can return the answer in any order.</p><p>&nbsp;</p><p><strong>Example 1:</strong></p><pre><strong>Input:</strong> nums = [2,7,11,15], target = 9↵<strong>Output:</strong> [0,1]<strong>Output:</strong> Because nums[0] + nums[1] == 9, we return [0, 1].</pre><p><strong>Example 2:</strong></p><pre><strong>Input:</strong> nums = [3,2,4], target = 6<strong>Output:</strong> [1,2]</pre><p><strong>Example 3:</strong></p><pre><strong>Input:</strong> nums = [3,3], target = 6<strong>Output:</strong> [0,1]</pre><p>&nbsp;</p><p><strong>Constraints:</strong></p><ul><li><code>2 &lt;= nums.length &lt;= 10<sup>5</sup></code></li><li><code>-10<sup>9</sup> &lt;= nums[i] &lt;= 10<sup>9</sup></code></li><li><code>-10<sup>9</sup> &lt;= target &lt;= 10<sup>9</sup></code></li><li><strong>Only one valid answer exists.</strong></li></ul>");
-  const [participantQuestionContent, setParticipantQuestionContent] = useState("");
+  const [yourQuestionContent, setYourQuestionContent] = useState({ title: "", body: "" });
+  const [participantQuestionContent, setParticipantQuestionContent] = useState({ title: "", body: "" });
   const [showYourQuestion, setShowYourQuestion] = useState(true);
   const [showParticipantQuestion, setShowParticipantQuestion] = useState(false);
   const [consoleOutputContent, setConsoleOutputContent] = useState("");
@@ -168,7 +168,12 @@ const Interview = () => {
       let participantObj = interview.participants.find(
         (o) => o._id !== userData.user._id
       );
+      let userObj = interview.participants.find(
+        (o) => o._id == userData.user._id
+      );
       setInterview(interview);
+      setParticipantQuestionContent({ title: participantObj.question.title, body: participantObj.question.body })
+      setYourQuestionContent({ title: userObj.question.title, body: userObj.question.body })
       setparticipant({ id: participantObj._id, name: participantObj.firstName + ' ' + participantObj.lastName });
       socket.current = io.connect(SocketUrl, {
         query: `room=${interview._id}`,
